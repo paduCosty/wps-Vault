c4421a96727d4dac0ea594f9e6e13e635e025194
cristian <andreea_adelina33@yahoo.com>
2023-07-10 21:23:19 +0300

    Added template for future customers

--- resources/js/Components/Customers/CustomerForm.vue
+++ resources/js/Components/Customers/CustomerForm.vue
@@ -1,11 +1,56 @@
 <script setup>
-
+ 
 </script>
 
 <template>
+//Aici facem tabelul Costumers
 
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>Contact Name</th>
+                    <th>Company Name</th>
+                    <th>Email</th>
+                    <th>VAT Number</th>
+                    <th>Status</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+      <tbody>
+        <tr v-for="customer in customers" :key="customer.user_id">
+          <td>{{ customer.user_id }}</td>
+          <td>{{ customer.contact_name }}</td>
+          <td>{{ customer.company_name }}</td>
+          <td>{{ customer.email }}</td>
+          <td>{{ customer.vat_number }}</td>
+          <td>{{ customer.status }}</td>
+          <td>{{ customer.type }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
 
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  padding: 8px;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  font-weight: bold;
+}
 </style>
+<template>
+  
+
+
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::get('/{any}', function () {
-//     return view('app');
-// })->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
 
 Auth::routes();
 
