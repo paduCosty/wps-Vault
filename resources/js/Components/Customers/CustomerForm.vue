5981f3b0704259448e32db9c340f1f578418d040
cristian <andreea_adelina33@yahoo.com>
2023-07-12 21:45:44 +0300

    Viitorul temmplate Costumers

--- resources/js/Components/Customers/CustomerForm.vue
+++ resources/js/Components/Customers/CustomerForm.vue
@@ -1,11 +1,55 @@
-<script setup>
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      axios.get('/api/customers')
+        .then(response => {
+          this.users = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
 </script>
 
+
 <template>
-
+    <table>
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Company Name</th>
+                <th>Contact Name</th>
+                <th>Email</th>
+                <th>VAT Number</th>
+                <th>Status</th>
+                <th>Type</th>
+                <th>Order Count</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user.user_id">
+                <td>{{ user.user_id }}</td>
+                <td>{{ user.company_name }}</td>
+                <td>{{ user.contact_name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.vat_number }}</td>
+                <td>{{ user.status }}</td>
+                <td>{{ user.type }}</td>
+                <td>{{ user.orderCount }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
-
-<style scoped>
-
-</style>
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api'], function () {
+
     Route::resource('users', UserController::class);
     Route::resource('customers', CustomerController::class);
     Route::resource('invoices', InvoiceController::class);
-});
+
